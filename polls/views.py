0cc463a62f090d341146cb102dac5257beb547de
mcatany <mcatany@hotmail.com>
2018-10-06 20:43:19 +0200

    Add more test and .css
    - Add style.css
    - Add more tests

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from .models import Question, Choice
 from django.urls import reverse
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -31,12 +32,22 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
+
+    ''' Return the last 5 published questions (not including those 
+    with future publication dates'''
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/details.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
